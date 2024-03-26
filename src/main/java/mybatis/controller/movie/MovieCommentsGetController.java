7bd075f0bb630ccbbb3ffd2772b2107e9e41948a
CurryToast <gusqjacjswo39@gmail.com>
2024-03-26 14:28:42 +0900

    영화 후기 페이지

--- src/main/java/mybatis/controller/movie/MovieCommentsGetController.java
+++ src/main/java/mybatis/controller/movie/MovieCommentsGetController.java
@@ -1,7 +1,9 @@
 package mybatis.controller.movie;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,14 +12,58 @@ import javax.servlet.http.HttpServletResponse;
 
 import mybatis.controller.Controller;
 import mybatis.dao.MovieCommentsDao;
+import mybatis.dao.MovieDao;
+import mybatis.vo.Movie;
 import mybatis.vo.MovieComments;
 
 public class MovieCommentsGetController implements Controller {
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		MovieCommentsDao dao = MovieCommentsDao.getInstance();
-		List<MovieComments> list = dao.selectAll();
-		request.setAttribute("list", list);
+		MovieDao dao = MovieDao.getInstance();
+		String type = request.getParameter("type");
+		List<Movie> list = null;
+
+		if (type == null) {
+	        list = dao.selectAll();
+		} else {
+			switch (type) {
+			case "all": 
+				list = dao.selectAll();
+				break;
+			case "ing": 
+				list = dao.selectCurrentMovies();
+				break;
+			case "later": 
+				list = dao.selectUpcomingMovies();
+				break;
+			case "end":
+				list = dao.selectEndMovies();
+				break;
+			default:
+				list = dao.selectAll();
+				break;
+			}
+		}
+
+		if (list.size() > 0) {
+			MovieCommentsDao comDao = MovieCommentsDao.getInstance();
+			List<MovieComments> comments = comDao.selectAll();
+			list.forEach((item) -> {
+				if (item.getComments() > 0) {
+					Map<String, String> cl = new HashMap<String, String>();
+					comments.forEach(cm -> {
+						if (cm.getMcode() == item.getMcode()) {
+							cl.put(cm.getWriter(), cm.getContent());
+						}
+					});
+
+					item.setCommentMap(cl);
+				}
+			});
+		}
+
+	    request.setAttribute("list", list);
+
 		RequestDispatcher dispatcher = request.getRequestDispatcher("comments.jsp");
 		dispatcher.forward(request, response);
 	}
